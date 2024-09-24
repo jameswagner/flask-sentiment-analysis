cadb09ace9970acfe4193bce3a51129ee6aa2526
jameswagner <jamesrwagner@gmail.com>
2024-09-23 19:11:23 -0500

    fix revision to None

--- alembic/versions/b38971189e5b_update_analysis_model.py
+++ alembic/versions/b38971189e5b_update_analysis_model.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'b38971189e5b'
-down_revision: Union[str, None] = '20f820b256a9'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
